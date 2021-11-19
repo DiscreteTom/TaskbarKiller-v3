c812cf732f97bc14ba2dc9acba6e14b44806c59b
DiscreteTom <discrete_tom@outlook.com>
2021-11-19 19:23:34 +0800

    update

--- TaskbarKiller-v3.cpp
+++ TaskbarKiller-v3.cpp
@@ -1,16 +1,20 @@
 #include <Windows.h>
 #include <WinUser.h>
 #include <wingdi.h>
-#include <stdio.h>
-#include <iostream>
 
-using namespace std;
+// hide command line window
+#pragma comment(linker, "/subsystem:\"windows\" /entry:\"mainCRTStartup\"")
+
+const int HOTKEY_ID = 1;
 
 double horizontalScale;
 double verticalScale;
+HWND taskbar;
+HRGN region;
+HRGN empty;
 
 // Ref: https://stackoverflow.com/questions/54912038/querying-windows-display-scaling
-void get_scale() {
+void get_display_scale() {
 	auto activeWindow = GetActiveWindow();
 	HMONITOR monitor = MonitorFromWindow(activeWindow, MONITOR_DEFAULTTONEAREST);
 
@@ -34,29 +38,65 @@ void get_scale() {
 	verticalScale = ((double)cyPhysical / (double)cyLogical);
 }
 
+void register_hot_key() {
+	RegisterHotKey(
+		NULL, // this thread will process the hotkey
+		HOTKEY_ID,
+		MOD_WIN, // win
+		VK_OEM_3  // ~
+	);
+}
+
+void unregister_hot_key() {
+	UnregisterHotKey(NULL, HOTKEY_ID);
+}
+
+void hide_taskbar() {
+	SetWindowRgn(taskbar, empty, true);
+}
+
+void show_taskbar() {
+	SetWindowRgn(taskbar, region, true);
+}
+
+void run() {
+	MSG msg = { 0 };
+	bool hidden = false;
+	while (GetMessage(&msg, NULL, 0, 0) != 0) // while msg is not WM_QUIT
+	{
+		if (msg.message == WM_HOTKEY) // process hot key
+		{
+			hidden = !hidden;
+			if (hidden)
+				hide_taskbar();
+			else
+				show_taskbar();
+		}
+	}
+}
+
 int main()
 {
-	get_scale();
+	get_display_scale();
+	register_hot_key();
 
 	// get taskbar handle
-	auto taskbar = FindWindowExA(NULL, NULL, "Shell_TrayWnd", NULL);
-	cout << taskbar << endl;
+	taskbar = FindWindowExA(NULL, NULL, "Shell_TrayWnd", NULL);
 
 	// store taskbar region
 	RECT rc;
 	GetWindowRect(taskbar, &rc);
-	auto region = CreateRectRgn(rc.left, rc.top, rc.right * horizontalScale, rc.bottom * verticalScale);
+	region = CreateRectRgn(rc.left, rc.top, rc.right * horizontalScale, rc.bottom * verticalScale);
 
-	// hide taskbar
-	auto empty = CreateRectRgn(0, 0, 0, 0);
-	if (SetWindowRgn(taskbar, empty, true) != 0) {
-		getchar();
-		// restore taskbar
-		SetWindowRgn(taskbar, region, true);
-	}
+	// create an empty region to hide taskbar
+	empty = CreateRectRgn(0, 0, 0, 0);
+
+	// handle hot key, hide/show taskbar
+	run();
 
 	// clean up
 	DeleteObject(empty);
 	DeleteObject(region);
+	unregister_hot_key();
 }
 
