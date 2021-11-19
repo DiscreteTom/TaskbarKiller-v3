53989d3abf197a84729b2ed91c8d1f3539245552
DiscreteTom <discrete_tom@outlook.com>
2021-11-19 20:30:16 +0800

    add debug output

--- TaskbarKiller-v3.cpp
+++ TaskbarKiller-v3.cpp
@@ -1,10 +1,15 @@
 #include <Windows.h>
 #include <WinUser.h>
 #include <wingdi.h>
-#include <stdio.h>
 
+#ifdef _DEBUG
+#include <stdio.h>
+#endif
+
+#ifndef _DEBUG
 // hide command line window
 #pragma comment(linker, "/subsystem:\"windows\" /entry:\"mainCRTStartup\"")
+#endif
 
 const int HOTKEY_ID = 1;
 
@@ -36,15 +41,27 @@ void get_display_scale() {
 	// Calculate the scaling factor
 	horizontalScale = ((double)cxPhysical / (double)cxLogical);
 	verticalScale = ((double)cyPhysical / (double)cyLogical);
+
+#ifdef _DEBUG
+	printf("horizontalScale: %f\n", horizontalScale);
+	printf("verticalScale: %f\n", verticalScale);
+#endif
 }
 
 void register_hot_key() {
-	RegisterHotKey(
+#ifdef _DEBUG
+	printf("registering hot key...");
+#endif
+	if (RegisterHotKey(
 		NULL, // this thread will process the hotkey
 		HOTKEY_ID,
 		MOD_WIN, // win
 		VK_OEM_3  // ~
-	);
+	)) {
+#ifdef _DEBUG
+		printf("done.\n");
+#endif
+	}
 }
 
 void unregister_hot_key() {
@@ -52,12 +69,18 @@ void unregister_hot_key() {
 }
 
 void hide_taskbar() {
+#ifdef _DEBUG
+	printf("hide\n");
+#endif
 	auto empty = CreateRectRgn(0, 0, 0, 0);
 	SetWindowRgn(taskbar, empty, true);
 	DeleteObject(empty);
 }
 
 void show_taskbar() {
+#ifdef _DEBUG
+	printf("show\n");
+#endif
 	auto region = CreateRectRgn(position.left, position.top, position.right * horizontalScale, position.bottom * verticalScale);
 	SetWindowRgn(taskbar, region, true);
 	DeleteObject(region);
@@ -79,17 +102,34 @@ void run() {
 	}
 }
 
-void format_position(){
+void format_position() {
 	if (position.left < 0) {
 		// taskbar is on the left, move right
-
+#ifdef _DEBUG
+		printf("taskbar is on the left.\n");
+		printf("original left: %d\n", position.left);
+		printf("original right: %d\n", position.right);
+#endif
 		position.right -= position.left;
 		position.left = 0;
+#ifdef _DEBUG
+		printf("corrected left: %d\n", position.left);
+		printf("corrected right: %d\n", position.right);
+#endif
 	}
 	if (position.bottom < 0) {
 		// taskbar is on the bottom, move up
+#ifdef _DEBUG
+		printf("taskbar is on the bottom.\n");
+		printf("original top: %d\n", position.top);
+		printf("original bottom: %d\n", position.bottom);
+#endif
 		position.top -= position.bottom;
 		position.bottom = 0;
+#ifdef _DEBUG
+		printf("corrected top: %d\n", position.top);
+		printf("corrected bottom: %d\n", position.bottom);
+#endif
 	}
 }
 
@@ -100,6 +140,9 @@ int main()
 
 	// get taskbar handle
 	taskbar = FindWindowExA(NULL, NULL, "Shell_TrayWnd", NULL);
+#ifdef _DEBUG
+	printf("taskbar handle: %d\n", taskbar);
+#endif
 
 	// get taskbar position
 	GetWindowRect(taskbar, &position);
